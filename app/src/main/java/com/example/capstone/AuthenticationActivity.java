8c9d1ae2ab13cd637b1b779182b36114e5aacad6
mollyxu <molly.xu@uwaterloo.ca>
2022-06-21 15:51:35 -0700

    working on login fragment :)

---
+++ app/src/main/java/com/example/capstone/AuthenticationActivity.java
@@ -0,0 +1,44 @@
+package com.example.capstone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+//public class AuthenticationActivity extends AppCompatActivity {
+//
+//    FragmentManager fragmentManager = getSupportFragmentManager();
+////    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//fragmentManager.beginTransaction()
+//        .replace(R.id.fragment_container, ExampleFragment.class, null)
+//    .setReorderingAllowed(true)
+//    .addToBackStack("name") // name can be null
+//    .commit();
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_authentication);
+//    }
+//
+//}
+
+public class AuthenticationActivity extends AppCompatActivity {
+    public AuthenticationActivity() {
+        super(R.layout.activity_authentication);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+        if (savedInstanceState == null) {
+            Bundle bundle = new Bundle();
+            bundle.putInt("some_int", 0);
+
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.authentication, LoginFragment.class, bundle)
+                    .commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/capstone/LoginFragment.java
@@ -0,0 +1,97 @@
+package com.example.capstone;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LoginFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoginFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+
+    private static final String TAG = "LoginFragment";
+
+    //
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+    private Button btnSignup;
+
+
+
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LoginFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LoginFragment newInstance(String param1, String param2) {
+        LoginFragment fragment = new LoginFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+        etUsername = getView().findViewById(R.id.et_username);
+        etPassword = getView().findViewById(R.id.et_password);
+        btnLogin = getView().findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick login button");
+//                String username = etUsername.getText().toString();
+//                String password = etPassword.getText().toString();
+//                loginUser(username, password);
+
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+
+}
