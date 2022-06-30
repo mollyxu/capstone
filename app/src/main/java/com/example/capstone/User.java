e5330294cfaec7faeb81d2c9108b9d8b890344f8
mollyxu <molly.xu@uwaterloo.ca>
2022-06-29 23:30:25 -0700

    Linked complete profile section to Parse and added school as a field

--- app/src/main/java/com/example/capstone/User.java
+++ app/src/main/java/com/example/capstone/User.java
@@ -9,12 +9,17 @@ import com.parse.ParseUser;
 @ParseClassName("CustomUser")
 public class User extends ParseObject {
     public static final String KEY_FIREBASE_UID = "firebase_uid";
+    public static final String KEY_FULL_NAME = "full_name";
 
     public String getFirebaseUid(){
         return getString(KEY_FIREBASE_UID);
     }
-
     public void setFirebaseUid(String firebase_uid){
+        put(KEY_FIREBASE_UID, firebase_uid);
+    }
+
+    public String getFullName() { return getString(KEY_FULL_NAME); }
+    public void setFullName(String firebase_uid){
         put(KEY_FIREBASE_UID, firebase_uid);
     }
 }
