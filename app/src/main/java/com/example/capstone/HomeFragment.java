833d227e7d9d1171869d8f542e370ac50b62fb15
mollyxu <molly.xu@uwaterloo.ca>
2022-07-06 16:31:51 -0700

    removed buttons for start session and join session from home fragment and converted them to FAB instead

--- app/src/main/java/com/example/capstone/HomeFragment.java
+++ app/src/main/java/com/example/capstone/HomeFragment.java
@@ -10,12 +10,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeFragment extends Fragment {
     private static final String TAG = "HomeFragment";
 
     Button btnJoinSession;
     Button btnStartSession;
+
+    FloatingActionButton mAddFab, mStartSessionFab, mJoinSessionFab;
+
+    TextView startSessionText, joinSessionText;
+
+    Boolean isAllFabsVisible;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -40,15 +50,49 @@ public class HomeFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        btnJoinSession = getActivity().findViewById(R.id.btn_join_session);
-        btnStartSession = getActivity().findViewById(R.id.btn_start_session);
-        btnStartSession.setOnClickListener(new View.OnClickListener() {
+        mAddFab = getActivity().findViewById(R.id.add_fab);
+        mStartSessionFab = getActivity().findViewById(R.id.start_session_fab);
+        mJoinSessionFab = getActivity().findViewById(R.id.join_session_fab);
+        startSessionText = getActivity().findViewById(R.id.start_session_text);
+        joinSessionText = getActivity().findViewById(R.id.join_session_text);
+
+        mStartSessionFab.setVisibility(View.GONE);
+        mJoinSessionFab.setVisibility(View.GONE);
+        startSessionText.setVisibility(View.GONE);
+        joinSessionText.setVisibility(View.GONE);
+
+        isAllFabsVisible = false;
+
+        mAddFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (!isAllFabsVisible) {
+                            mStartSessionFab.show();
+                            mJoinSessionFab.show();
+                            startSessionText.setVisibility(View.VISIBLE);
+                            joinSessionText.setVisibility(View.VISIBLE);
+                            isAllFabsVisible = true;
+                        } else {
+                            mStartSessionFab.hide();
+                            mJoinSessionFab.hide();
+                            startSessionText.setVisibility(View.GONE);
+                            joinSessionText.setVisibility(View.GONE);
+                            isAllFabsVisible = false;
+                        }
+                    }
+                });
+
+        mStartSessionFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i(TAG, "onClick start session button");
                 ((HomescreenActivity)getActivity()).replaceFragment(R.id.homescreen, StartSessionFragment.class);
             }
         });
+
+
+
     }
 
 }
